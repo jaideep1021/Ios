f7ab46743896a2b42a0196002c410a8f255d35e7
Jaideep Manupati <jaideepmanupati21@gmail.com>
2022-09-14 23:06:02 +0530

    Initial Commit

---
+++ Ios/IosApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosApp.swift
+//  Ios
+//
+//  Created by Jaideep Manupati on 14/09/22.
+//
+
+import SwiftUI
+
+@main
+struct IosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
